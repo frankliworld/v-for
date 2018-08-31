466212efb01d042d6f45c8a2bc814b8c00cba88f
frankliworld <frankliworld@gmail.com>
2018-08-30 21:25:45 -0500

    v-for
    subida 1

---
+++ js/vue.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var scroll = new Vue({
+    el: '#scroll',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
